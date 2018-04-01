39f68c67ba47e3c4a0c603ed36dc9b43a7f02d83
Daniel Javorszky <daniel.javorszky@liferay.com>
2018-04-01 15:36:44 +0200

    Rearrange

--- topic.go
+++ topic.go
@@ -70,3 +70,88 @@ func doAddTopic(topic *Topic) {
 	topics[topic.Name] = topic
 	rw.Unlock()
 }
+
+// AddUser adds a user to the topic. If a user with the name already exists, an error
+// is returned.
+func (t *Topic) AddUser(name string) (*User, error) {
+	if name == "" {
+		return nil, fmt.Errorf("name is required")
+	}
+
+	if t.userExists(name) {
+		return nil, fmt.Errorf("user with name %q already exists", name)
+	}
+
+	user := &User{
+		Name:    name,
+		devices: make(map[string]*Device, 0),
+	}
+
+	t.doAddUser(user)
+
+	return user, nil
+}
+
+// RegisterDevice registers a Device to a user. Creates the user if it doesn't
+// exist yet. Fails if User already has a device with the same DeviceID.
+func (t *Topic) RegisterDevice(d *Device) error {
+	if err := d.validate(); err != nil {
+		return fmt.Errorf("validation failed: %v", err)
+	}
+
+	user := t.getUser(d.UserID)
+
+	if user.deviceExists(d.DeviceID) {
+		return fmt.Errorf("device %q already exists for user", d.DeviceID)
+	}
+
+	user.doAddDevice(d)
+
+	return nil
+}
+
+// UnregisterDevice removes the device from the User.
+func (t *Topic) UnregisterDevice(userID, deviceID string) {
+	t.getUser(userID).deleteDevice(deviceID)
+}
+
+func (t *Topic) getUser(name string) *User {
+	t.rw.RLock()
+	user, ok := t.users[name]
+	t.rw.RUnlock()
+
+	if !ok {
+		user = &User{
+			Name:    name,
+			devices: make(map[string]*Device, 0),
+		}
+
+		t.doAddUser(user)
+	}
+
+	return user
+}
+
+func (t *Topic) userExists(name string) bool {
+	t.rw.RLock()
+	defer t.rw.RUnlock()
+	_, ok := t.users[name]
+
+	return ok
+}
+
+func (t *Topic) doAddUser(user *User) {
+	t.rw.Lock()
+	t.users[user.Name] = user
+	t.rw.Unlock()
+}
+
+func (t *Topic) doDeleteUser(user *User) {
+	t.rw.Lock()
+	delete(t.users, user.Name)
+	t.rw.Unlock()
+
+	for _, d := range user.devices {
+		user.deleteDevice(d.DeviceID)
+	}
+}
